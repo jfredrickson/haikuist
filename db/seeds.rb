212205c2f1b041c72375c09e64474ccbd26763b1
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2016-04-18 11:33:19 -0400

    add dev seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+  Haiku.create([
+    { content: "Green frog,\nIs your body also\nfreshly painted?" },
+    { content: "Sick and feverish\nGlimpse of cherry blossoms\nStill shivering." },
+    { content: "Without flowing wine\nHow to enjoy lovely\nCherry blossoms?" },
+    { content: "Fallen sick on a journey,\nIn dreams I run wildly\nOver a withered moor." },
+    { content: "An old pond!\nA frog jumps in-\nThe sound of water." },
+    { content: "The first soft snow!\nEnough to bend the leaves\nOf the jonquil low." },
+    { content: "Autumn moonlight-\na worm digs silently\ninto the chestnut." },
+    { content: "A summer river being crossed\nhow pleasing\nwith sandals in my hands!" },
+    { content: "O snail\nClimb Mount Fuji,\nBut slowly, slowly!" },
+    { content: "The crow has flown away:\nswaying in the evening sun,\na leafless tree." },
+  ])
+end
